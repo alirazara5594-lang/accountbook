955eeb808773c9e7db5a73388e5ddbfa65fc9ef3
alirazara5594-lang <aliraza.ra5594@gmail.com>
2026-08-12 02:07:31 +0500

    completed

--- backend/obj/Debug/net10.0/Zenabook.Api.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/Zenabook.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Accountbook.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b962e4164f61ff6fe8caad06b006dce4273332b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+797554a07ecec10bcb2c3c76f0b6c45530c39755")]
 [assembly: System.Reflection.AssemblyProductAttribute("Accountbook.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Accountbook.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
