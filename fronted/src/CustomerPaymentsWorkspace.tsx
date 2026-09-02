a89582e5bc205064f06d34a46b7ccdf3b3b67d97
alirazara5594-lang <aliraza.ra5594@gmail.com>
2026-09-03 02:37:30 +0500

    Update CustomerPaymentsWorkspace.tsx

--- fronted/src/CustomerPaymentsWorkspace.tsx
+++ fronted/src/CustomerPaymentsWorkspace.tsx
@@ -105,7 +105,7 @@ export function CustomerPaymentsWorkspace({
 
   const loadData = () => {
     fetchAll(activeEntityId);
-    fetchCompanies();
+    companyStore.fetchCompanies();
   };
 
   useEffect(() => {
